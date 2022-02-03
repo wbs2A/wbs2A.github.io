f0968be3302ab8e8f59e2f03bc6f8636d79fc14f
wbs2A <34480299+wbs2A@users.noreply.github.com>
2022-02-03 01:25:54 -0300

    Update tagueamento.js

--- case-dp6/js/tagueamento.js
+++ case-dp6/js/tagueamento.js
@@ -5,31 +5,39 @@
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-5SMMFB7G19');
 
-  gtag('config', 'G-5SMMFB7G19');
-
+let label_name    = null;
+let category_name = null;
+let action_name   = null;
 
  $('.card-montadoras').on('click',function(e){
     var card_name = $(this).attr('data-name')
     console.log(card_name, 'mandei')
+    label_name    = card_name
+    category_name = 'analise'
+    action_name   = 'ver_mais'
     gtag('event', 'click', {
         eventCategory: 'BT CARD MONTADORA',
         eventLabel: card_name
     });
 
-        gtag('event', 'ver_mais', {
-        'send_to': [
-          'GTM-NTD9D6T',
-          'G-5SMMFB7G19'
-        ],
-        'items': {
-          'category'  : 'EVENTO CARD MONTADORAS',
-          'Categoria' : 'analise',
-          'Ação'      : 'ver_mais',
-          'Rótulo': $(this).attr('data-name')
-        }
+    gtag('event', 'ver_mais', {
+    'send_to': [
+      'GTM-NTD9D6T',
+      'G-5SMMFB7G19'
+    ],
+    'items': {
+      'category'  : 'EVENTO CARD MONTADORAS',
+      'Categoria' : 'analise',
+      'Ação'      : 'ver_mais',
+      'Rótulo'    : $(this).attr('data-name'),
+      'label_name'    : card_name,
+      'category_name' : 'analise',
+      'action_name'   : 'ver_mais'
+    }
     });
 
  })
